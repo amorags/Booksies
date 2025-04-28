beb89a69028734005d4e072d7c355f9f917a35ce
amorags <alex.stengaard@gmail.com>
2025-04-28 11:13:33 +0200

    migrations

---
+++ BookList/Src/Migrations/20250426153947_AddCategoryToBook.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookList.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Category",
+                table: "Books",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PageCount",
+                table: "Books",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "PageCount",
+                table: "Books");
+        }
+    }
+}
---
+++ BookList/Src/Migrations/20250426154443_AddImageToBook.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookList.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Publishers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CoverUrl",
+                table: "Books",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Publishers");
+
+            migrationBuilder.DropColumn(
+                name: "CoverUrl",
+                table: "Books");
+        }
+    }
+}
